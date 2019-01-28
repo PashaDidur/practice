4f70037fa389c3c71c2112d7f7ecd096c0409755
Pasha Didur <pashadidur@gmail.com>
2019-01-28 23:31:30 +0200

    Study_site

---
+++ study_site/template/mail.php
@@ -0,0 +1,14 @@
+<?php
+
+$recepient = "youmail@ya.ru";
+$sitename = "Назва сайту";
+
+$name = trim($_GET["name"]);
+$phone = trim($_GET["phone"]);
+$text = trim($_GET["text"]);
+
+$pagetitle = "Нова заявка із сайту \"$sitename\"";
+$message = "Імя: $name \nТелефон: $phone \nТекст: $text";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
